f3ee3a172d83b786c405a871de26cfbbffe25cfe
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-04-13 16:08:46 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
 
 
   # route for loging in a student
-    post "/student_login", to :"sessions#logStudent" 
+    post "/studentlogin", to:"sessions#logStudent" 
   # route to logout
-    delete "/logout", to :"sessions#destroy"
+    delete "/logout", to:"sessions#destroy"
 
   # Defines the root path route ("/")
   # root "articles#index"
