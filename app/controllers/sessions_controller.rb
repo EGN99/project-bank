cd7bc5b96ce9dc874db11a023ff8b5916c6066a4
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-04-13 13:05:17 +0300

    add create and destroy session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,38 @@
 class SessionsController < ApplicationController
+
+ # logging the Admin in
+
+    def logAdmin
+        # insert code here
+        
+    end
+
+
+# logging the student in
+
+    def logStudent
+
+        student = Student.find_by(username: login_params[:username])
+        if student&.authenticate(login_params[:password])
+            session[:user_id]= student.id
+            render json: student, status: :accepted
+        else
+            render json: { error: "Invalid username or password"}, status: :not_found
+        end
+        
+    end
+
+
+# loging out for both the student and the admin
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+  private
+
+    def login_params
+        params.permit(:username, :password)
+    end
 end
