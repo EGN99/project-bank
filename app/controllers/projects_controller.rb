c0d3e9a3bfdcc3409541b1b7c433265dd92b5375
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-04-18 15:42:30 +0300

    modify projects_controller.rb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,34 @@
 class ProjectsController < ApplicationController
+    before_action :set_course
+    before_action :set_project, only: [:show]
+
+    # GET /courses/:id/projects
+    def index
+        @projects = @course.projects
+        render json: @Projects 
+    end
+
+    # POST /courses/:id/projects
+    def create
+        @projects = @courses.projects.build(project_params)
+
+        if @project.save
+            render json: @project, status: :created
+        else
+            render json: @project.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def set_course
+        @course = Course.find(params[:course_id])
+    end
+
+    def set_project
+        @project = @course.projects.find(params[:id])
+    end
+
+    def project_params
+        params.require(:project).permit(:name, :description, :github_link)
+    end
 end
