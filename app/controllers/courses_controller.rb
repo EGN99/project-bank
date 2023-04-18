da180a945839d2632bf7cc2a628dfc14f8963dee
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-04-18 15:21:59 +0300

    modify courses_controller.rb

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,20 @@
 class CoursesController < ApplicationController
+    before_action :set_course, only: [:show]
+
+    # GET /courses
+    def index
+        @courses = Course.all 
+        render json: @courses 
+    end
+    
+    # GET /courses/:id
+    def show 
+        render json: @course 
+    end
+
+    private
+    def set_course
+        @course = Course.find(params[:id])
+    end
+
 end
