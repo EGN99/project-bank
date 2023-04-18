57edbc45178b4ddca719ea02003e1579ec9892f8
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-04-18 16:09:47 +0300

    modify student_controller.rb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,36 @@
 class StudentsController < ApplicationController
+    before_action :authenticate_admin!
+
+    # GET /students
+    def index
+    @students = Student.all  
+    render json: @students 
+    end
+
+    
+    def show
+        render json: @student
+    end
+
+    # POST /students
+    def create
+    @student = Student.new(student_params)
+
+    if @student.save
+        render json: @student, status: :created, location: @student
+    else
+        render json: @student.errors, student: :unprocessed entity
+    end
+end
+
+private
+def set_student
+    @student = Student.find(params[:id])
+end
+
+def student_params
+    params.require(:student).permit(:username, :email, :password)
+end
+
+
 end
