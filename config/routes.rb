b893dfe924708e5bf33c47ac27be1de6ffe73634
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-04-18 15:03:22 +0300

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
   resources :cohorts
   resources :students
 
-  resources :courses, only: [:index, :show]do
-resources :projects, only: [:index, :create]
-  end
 
 end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
