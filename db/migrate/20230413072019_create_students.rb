a0635d5aa526236e034b7949ed70db6bc08784ef
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-04-18 09:17:57 +0300

    change name to username

--- db/migrate/20230413072019_create_students.rb
+++ db/migrate/20230413072019_create_students.rb
@@ -1,9 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[7.0]
   def change
     create_table :students do |t|
-      t.string :name
+      t.string :username
       t.string :email
-      t.references :cohort, null: false, foreign_key: true
       t.string :password_digest
 
       t.timestamps
