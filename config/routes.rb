b81b5c04bfad4686383c7e215848eff185e9596d
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-04-18 14:39:34 +0300

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,31 @@ Rails.application.routes.draw do
   resources :projects
   resources :cohorts
   resources :students
+
+  resources :courses, only: [:index, :show]do
+resources :projects, only: [:index, :create]
+  end
+
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  #route for loging in an admin
+  post "/admin_login", to: "sessions#loginAdmin"
+  #route to logout
+  delete "/logout", to: "sessions#destroy"
+
+  # devise_for :admins
+
+  namespace :api do
+    namespace :v1 do
+    resources :admins do 
+    resources :cohorts 
+    resources :projects
+    end
+  end
+end
+
+
 end
