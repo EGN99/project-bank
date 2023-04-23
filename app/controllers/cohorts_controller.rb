f088ea06a86c2f0a3c608af7be6719376f11124f
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-04-23 15:31:23 +0300

    modify cohorts_controller.rb

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -6,26 +6,26 @@ class CohortsController < ApplicationController
         render json: @cohorts
     end
     
-#     def create_cohort
-#         cohort = Cohort.new(cohort_params)
-#         if cohort.save
-#             render json: {status: 'success', message: 'cohort created successefully'}, :status :successefully
-#         else
-#             render json: {status: 'error', message: 'cohort creation failed'}, :status :error
-#         end
-#     end
+    def create_cohort
+        cohort = Cohort.new(cohort_params)
+        if cohort.save
+            render json: {status: 'success', message: 'cohort created successefully'}, status: :successefully
+        else
+            render json: {status: 'error', message: 'cohort creation failed'}, status: :error
+        end
+    end
 
-#     def delete_cohort
-#         cohort = Cohort.find(params[:id])
-#         if cohort.destroy
-#             render json: {status: 'success', message: 'cohort deleted successfully'}, :status :success
-#         else
-#             render json: {status: 'error', message: 'cohort deletion failed'}, :status :error
-#         end
-#     end
+    def delete_cohort
+        cohort = Cohort.find(params[:id])
+        if cohort.destroy
+            render json: {status: 'success', message: 'cohort deleted successfully'}, status: :success
+        else
+            render json: {status: 'error', message: 'cohort deletion failed'}, status: :error
+        end
+    end
 
-# private 
-# def cohort_params
-#     params.require(:cohort).permit(:name, :course, :no_of_students)
-# end
+private 
+def cohort_params
+    params.require(:cohort).permit(:name, :course, :no_of_students)
+end
 end
