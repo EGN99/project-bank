028fc6e51034f5ea9be5d00abe665f13a2596085
isaac <isaactonyloi@gmail.com>
2023-04-24 11:04:02 +0300

    admin, cohorts, courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,39 @@
 Rails.application.routes.draw do
     resources :students, only: [:index, :password_reset]
 
-  
+    #routes for course
+    resources :courses
+    
+    #routes for cohorts
+    resources :courses do
+      resources :cohorts
+    end
+     
+
+
+    #route for getting projects under each course
     resources :courses do
       resources :projects
     end
     # route for loging in a student
     post "/studentlogin", to:"sessions#logStudent" 
+
+    #get list of all students in the system
+    get "/students", to:"students#index"
+
+    #route for studnt signup
+    post "/studentssignup", to:"students#create" 
+
+    #get list of admins
+    get "/admins", to:"admins#index"
+
+    #route for admin signup
+    post "/adminssignup", to:"admins#create" 
+
     # route to logout
     delete "/logout", to:"sessions#destroy"
 
     post '/password_reset', to: 'sessions#password_reset'
-    get 'password_reset/:id/edit', to: 'password_reset#edit'
-    patch 'password_reset/:id', to: 'password_reset#update'
-  
-  
+    
   end
   
